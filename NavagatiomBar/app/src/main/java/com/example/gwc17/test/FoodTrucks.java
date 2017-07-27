02bd71e68b655f78c9536b6b0988f2f2b2ee4848
jhoffman1 <jhoffman40@gatech.edu>
2017-07-27 14:07:23 -0700

    add map icon to all pages except about us

--- NavagatiomBar/app/src/main/java/com/example/gwc17/test/FoodTrucks.java
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/FoodTrucks.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,6 +14,12 @@ import android.view.ViewGroup;
 
 public class FoodTrucks extends AppCompatActivity {
     View myView;
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     @Nullable
 //    @Override
