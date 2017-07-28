ceabe85d49b0204fc2a29493f626721eb803995a
Emily San Juan <sj.emily@yahoo.com>
2017-07-28 14:08:43 -0700

    google maps good

--- NavagatiomBar/app/src/main/java/com/example/gwc17/test/FoodMap.java
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/FoodMap.java
@@ -12,8 +12,10 @@ import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 //import android.view.View.OnClickListener;
@@ -30,21 +32,28 @@ public class FoodMap extends AppCompatActivity implements OnMapReadyCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            mMap.setMyLocationEnabled(true);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+//            mMap.setMyLocationEnabled(true);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-33.852, 151.211);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+
+
     }
 
 
--- NavagatiomBar/app/src/main/java/com/example/gwc17/test/MapsActivity.java
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.example.gwc17.test;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -22,6 +26,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            mMap.setMyLocationEnabled(true);
     }
 
 
@@ -42,5 +49,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng KazuNori = new LatLng(34.047700, -118.247882);
+        mMap.addMarker(new MarkerOptions().position(KazuNori).title("KazuNori").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KazuNori, 12.0f));
+
+        LatLng GrandCentralMarket =new LatLng(34.050655, -118.248795);
+        mMap.addMarker(new MarkerOptions().position(GrandCentralMarket).title("Grand Central Market").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GrandCentralMarket, 12.0f));
+
+        LatLng Birdies =new LatLng(34.041769, -118.259048);
+        mMap.addMarker(new MarkerOptions().position(Birdies).title("Birdies").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Birdies, 12.0f));
+
+        LatLng UmamiBurgerBroadway =new LatLng(34.042400, -118.255606);
+        mMap.addMarker(new MarkerOptions().position(UmamiBurgerBroadway).title("Umami Burger Broadway").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UmamiBurgerBroadway, 12.0f));
+
+        LatLng GoldenStateCafe =new LatLng(34.078998, -118.361336);
+        mMap.addMarker(new MarkerOptions().position(GoldenStateCafe).title("GoldenStateCafe").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GoldenStateCafe, 12.0f));
+
+        LatLng Animal =new LatLng(34.079217, -118.361604);
+        mMap.addMarker(new MarkerOptions().position(Animal).title("Animal Restaurant").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Animal, 12.0f));
+
+        LatLng Burgerlords =new LatLng(34.065207, -118.237149);
+        mMap.addMarker(new MarkerOptions().position(Burgerlords).title("Burgerlords").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Burgerlords, 12.0f));
+
+        LatLng Delilah =new LatLng(34.091064, -118.363581);
+        mMap.addMarker(new MarkerOptions().position(Delilah).title("Delilah").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Delilah, 12.0f));
+
+        LatLng Alma =new LatLng(34.095928, -118.371018);
+        mMap.addMarker(new MarkerOptions().position(Alma).title("Alma").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Alma, 12.0f));
     }
 }
