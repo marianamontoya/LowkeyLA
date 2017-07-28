365b443a4e7697cee4da7dcd951d691f5beb958d
Emily San Juan <sj.emily@yahoo.com>
2017-07-28 14:58:36 -0700

    added maps to all

--- NavagatiomBar/app/src/main/java/com/example/gwc17/test/CoolPlacesMap.java
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/CoolPlacesMap.java
@@ -1,10 +1,45 @@
 package com.example.gwc17.test;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by jessicahoffman on 7/27/17.
  */
 
-public class CoolPlacesMap extends AppCompatActivity {
-}
+public class CoolPlacesMap extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+//            mMap.setMyLocationEnabled(true);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+
+        LatLng KazuNori = new LatLng(34.047700, -118.247882);
+        mMap.addMarker(new MarkerOptions().position(KazuNori).title("KazuNori").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KazuNori, 12.0f));
+        {
+}}}
